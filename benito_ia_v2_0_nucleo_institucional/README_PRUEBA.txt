202718cc55b5f338f343869af36893231475193b
Escuela Secundaria N.º 31 Benito Juárez <escsec31benitojuarez@gmail.com>
2026-07-22 12:43:53 -0300

    A10

---
+++ benito_ia_v2_0_nucleo_institucional/README_PRUEBA.txt
@@ -0,0 +1,15 @@
+BENITO IA v2.0 — CÓMO PROBAR
+
+1. Descomprimir el ZIP.
+2. Abrir la carpeta.
+3. Hacer doble clic en index.html.
+4. Abrir Benito IA abajo a la derecha.
+
+Consultas sugeridas:
+- Soy de 2.º B
+- ¿Dónde están mis materiales?
+- Tengo una prueba y no sé estudiar
+- Necesito el teléfono
+- Quiero conocer la escuela
+
+No modifica el sitio público.
---
+++ benito_ia_v2_0_nucleo_institucional/benito-ia/config.js
@@ -0,0 +1,24 @@
+window.BENITO_IA_CONFIG={
+ version:'2.0.0-nucleo-institucional',
+ assistantName:'Benito IA',
+ assistantSubtitle:'Asistente pedagógico y digital',
+ schoolName:'Escuela Secundaria N.º 31 “Benito Juárez”',
+ storageKey:'benitoIA_v2_state',
+ typingDelay:420,maxHistory:40,fuzzyThreshold:.68,provider:'local',
+ drives:{
+  '1A':'https://drive.google.com/drive/folders/1GE2AULDtGoTl5LD3pB_NPHKIQ-OJeu2i?usp=sharing',
+  '1B':'https://drive.google.com/drive/folders/1Y-FL9tGLNdAIZTEgpWvhGFEqSC1dqb3B?usp=sharing',
+  '2A':'https://drive.google.com/drive/folders/1Jibq7SqqrnxAo56xE7IIwSg3BTpvRpZv?usp=sharing',
+  '2B':'https://drive.google.com/drive/folders/1odPjWQ4YFyiYJCwJS-GIQAMBomgGN2_j?usp=sharing',
+  '3':'https://drive.google.com/drive/folders/19dNH2PGVW27QyGeTHpK9BiPegCziAzZs?usp=sharing',
+  '4':'https://drive.google.com/drive/folders/1sAlEO7mq-fhoRD0ediFmQIYvYcnKD5Wf?usp=sharing',
+  '5':'https://drive.google.com/drive/folders/1Et7h5kwBIJd19L0VW_y3uS8cSVNFeVDU?usp=sharing',
+  '6':'https://drive.google.com/drive/folders/1gAc-6ZfTB8FTzx2nz7u5hVyW2WGmxhsQ?usp=sharing'
+ },
+ profiles:{
+  estudiante:{label:'Estudiante',icon:'🎓'},
+  familia:{label:'Familia',icon:'👨‍👩‍👧'},
+  docente:{label:'Docente',icon:'👩‍🏫'},
+  visitante:{label:'Visitante',icon:'👥'}
+ }
+};
